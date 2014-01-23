fc70d052e460be529338236ca1aa7d1f4c25245f
Igor Fontana <rogi@skylittlesystem.org>
2014-01-23 03:58:53 -0200

    Add LICENSE and copyright notice.

--- sauce/ui/ui.c
+++ sauce/ui/ui.c
@@ -1,4 +1,26 @@
 /*
+ * Copyright (C) 2014 Igor de Sant'Ana Fontana.
+ * <rogi@skylittlesystem.org>
+ *
+ * This file is part of engine2d.
+ *
+ * engine2d is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * engine2d is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with engine2d.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
  *
  *
  *
